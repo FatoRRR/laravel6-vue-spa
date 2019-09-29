391a87055addd4983512d61a97ba33e652cf1c26
FatmirRragomaj <fatrrag@gmail.com>
2019-09-29 21:07:40 +0200

    Add registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,13 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\User;
-use App\Person;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
+    public function register(RegisterRequest $request)
+    {
+        // $address = new Address([
+        //     'country_id' => 82,
+        //     'postal_code' => $request->postalCode,
+        //     'city' => $request->city,
+        //     'street' => $request->street,
+        //     'housenumber' => $request->housenumber
+        // ]);
+        // $address->save();
+
+        // $person = new Person([
+        //     'address_id' => $address->id,
+        //     'first_name' => $request->firstName,
+        //     'last_name' => $request->lastName,
+        //     'birthday' => $request->birthday,
+        //     'birthday_place' => $request->birthdayPlace,
+        // ]);
+        // $person->save();
+
+        $user = new User([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            // 'person_id' => $person->id,
+            // 'picture' => $request->picture
+        ]);
+        $user->save();
+
+        return $user;
+    }
+
     public function login(LoginRequest $request)
     {
         $credentials = $request->only('email', 'password');
--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements JWTSubject
     ];
 
     protected $fillable = [
-        'name', 'email', 'password',
+        'username', 'email', 'password',
     ];
 
     protected $hidden = [
